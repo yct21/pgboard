6a5fe468ab939e37d78bef85d2cf347627a6c0e5
yct21 <yct21@12tcy.com>
2016-02-26 20:52:49 +0800

    add path in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pgboard, Pgboard.Endpoint,
   http: [port: 4002],
-  url: [host: "localhost", port: 4002]
+  url: [host: "localhost", path: "/pgboard", port: 4002]
 
 # Do not print debug messages in production
 config :logger, level: :info
