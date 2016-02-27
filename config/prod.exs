0d11a4540a597770e1de8ee06b451885e26e160f
yct21 <yct21@12tcy.com>
2016-02-27 20:44:28 +0800

    add menifest back

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pgboard, Pgboard.Endpoint,
   http: [port: 4002],
-  url: [host: "localhost", path: "/pgboard", port: 4002]
+  url: [host: "localhost", path: "/pgboard", port: 4002],
+  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
