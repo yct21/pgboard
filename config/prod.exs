4c6d84699ffb01bcf8fbfab08fd93deeeb1d4524
yct21 <yct21@12tcy.com>
2016-02-27 23:08:47 +0800

    change host name

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pgboard, Pgboard.Endpoint,
   http: [port: 4002],
-  url: [host: "localhost", path: "/pgboard", port: 4002],
+  url: [host: "sanguobansha.com", path: "/pgboard", port: 4002],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
