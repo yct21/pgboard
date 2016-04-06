fde61181c061af566448df3d9348b694c1b77251
yct21 <yct21@12tcy.com>
2016-04-07 07:49:14 +0800

    Implement refill method in card_deck.

--- backend/game/card_deck.ex
+++ backend/game/card_deck.ex
@@ -54,25 +54,71 @@ defmodule Pgboard.Game.CardDeck do
   @doc """
   Basic process to initialize a card deck.
 
-  According to the rule, plant 3-10 would be placed in plant market,
-  following by card 13,
-  then all other cards shuffled with random plants removed(3 players: 8, 4 players: 4, above: 0).
+  According to the rule, plant 3-10 would be placed in plant market in order,
+  followed by card 13,
+  with all other cards shuffled while removing some plants randomly (3 players: 8, 4 players: 4, above: 0).
   Finally put Step 3 card to the bottom of deck.
 
   Each map may have specific rule of how to initialize a deck.
   """
   def basic_deck(player_amount) do
     plant_market = Enum.to_list(3..10)
-    plant_amount_to_remove = case player_amount do
-      3 -> 8
-      4 -> 4
-      5 -> 0
-      6 -> 0
-    end
+    plant_amount_to_remove =
+      case player_amount do
+        3 -> 8
+        4 -> 4
+        5 -> 0
+        6 -> 0
+      end
 
     remain_plants = (Map.keys(@plants) -- plant_market) -- [13]
     remain_deck = Enum.take_random(remain_plants, Enum.count(remain_plants) - plant_amount_to_remove)
 
     plant_market ++ [13] ++ remain_deck ++ [:step3]
+  end
+
+  @doc """
+  Refills the market when any cards is taken.
+  According to the rule, any plant in market should have id more than max plant amount of any player.
+
+  Returns processed {available_market, future_market, card_deck}.
+  """
+  def refill_market(plant_market, card_deck, available_amount, future_amount, max_plant_amount) do
+    market_plants = plant_market.available_market ++ plant_market.future_market
+    market_plants_amount = available_amount + future_amount
+    refill_amount = market_plants_amount - Enum.count(market_plants)
+
+    cond do
+      # Unnecessary to refill.
+      refill_amount <= 0 -> {plant_market, card_deck}
+      # Or...
+      true ->
+        {_discarded_plants, valid_plants} = Enum.split_while(card_deck, &(&1 != :step3 && &1 <= max_plant_amount))
+        plants_to_refill = Enum.take valid_plants, refill_amount
+
+        processed_card_deck =
+          case Enum.any?(plants_to_refill, &(&1 == :step3)) do
+            false ->
+              valid_plants -- plants_to_refill
+            true ->
+              # When step3 card appears, remaining cards should be shuffled.
+              Enum.shuffle(valid_plants -- plants_to_refill)
+          end
+
+        refilled_market_plants = sort_plants(market_plants ++ plants_to_refill)
+        {refilled_available_market, refilled_future_market} = Enum.split refilled_market_plants, available_amount
+
+        {refilled_available_market, refilled_future_market, processed_card_deck}
+    end
+  end
+
+  defp sort_plants(plants) do
+    Enum.sort plants, fn(plant1, plant2) ->
+      cond do
+        plant1 == :step3 -> false
+        plant2 == :step3 -> true
+        true -> plant1 <= plant2
+      end
+    end
   end
 end
---
+++ test/backend/game/card_deck_test.exs
@@ -0,0 +1,42 @@
+defmodule Pgboard.Game.CardDeckTest do
+  use ExUnit.Case, async: true
+  alias Pgboard.Game.CardDeck
+
+  test "It could get plants info by id" do
+    assert CardDeck.plant(3) == {2, :oil, 1}
+    assert CardDeck.plant(50) == {0, :ecological, 6}
+    assert CardDeck.plant(2) == nil
+    assert CardDeck.plant(41) == nil
+    assert CardDeck.plant(:step3) == nil
+  end
+
+  test "It could initialize a basic deck" do
+    card_deck = CardDeck.basic_deck(4)
+
+    assert Enum.count(card_deck) == 43 - 4
+    assert Enum.take(card_deck, 9) == [3, 4, 5, 6, 7, 8, 9, 10, 13]
+    assert List.last(card_deck) == :step3
+    assert Enum.uniq(card_deck) == card_deck
+
+    Enum.each card_deck, fn(card) ->
+      cond do
+        is_atom(card) -> assert card == :step3
+        is_integer(card) -> assert(card >= 3 && card <= 50 && not(card in [41, 43, 45, 47, 48, 49]))
+      end
+    end
+  end
+
+  test "It could refill a plant market" do
+    plant_market = %{
+      available_market: [4, 5],
+      future_market: [17, 23, 34, 45]
+    }
+    card_deck = [10, 18, :step3, 42, 35]
+
+    {refilled_available_market, refilled_future_market, processed_card_deck} = CardDeck.refill_market(plant_market, card_deck, 5, 3, 10)
+
+    assert refilled_available_market == [4, 5, 17, 18, 23]
+    assert refilled_future_market == [34, 45, :step3]
+    assert Enum.sort(processed_card_deck) == [35, 42]
+  end
+end
